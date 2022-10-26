788c505f8a1eec04a50e9ccecb9892a25bf20fda
Oleksii26 <Kolesnik26@ukr.net>
2022-10-26 19:14:30 +0300

    save

--- src/components/Statistics/FeedbackOptions.jsx
+++ src/components/Statistics/FeedbackOptions.jsx
@@ -1,11 +1,10 @@
-
-export const FeedbackOptions = () => {
-    return {
-        <div className={css.btn__list}>
-        <button className={css.btn} onClick={this.incrementGood}>Good</button>
-        <button className={css.btn} onClick={this.incrementNeutral}>Bad</button>
-        <button className={css.btn} onClick={this.incrementBad}>Neutral</button>
-        <button onClick={this.toggleMenu}>togle</button>
+import css from './Statistics.module.css'
+export const FeedbackOptions = ({incrementGood1, incrementNeutral1, incrementBad1}) => {
+    return  <div className={css.btn__list}>
+        <button className={css.btn} onClick={incrementGood1}>Good</button>
+        <button className={css.btn} onClick={incrementNeutral1}>Neutral</button>
+        <button className={css.btn} onClick={incrementBad1}>Bad</button>
+        {/* <button onClick={this.toggleMenu}>togle</button> */}
     </div> 
-    }
+    
 }
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,5 +1,7 @@
 import css from './Statistics.module.css'
 import React from 'react'
+// import { FeedbackOptions } from './FeedbackOptions'
+
 
 
 class Statistics extends React.Component {
@@ -9,16 +11,19 @@ class Statistics extends React.Component {
             good: 0,
             neutral: 0,
             bad: 0,
-            open: false
+            // open: false
         }
     }
 
-    incrementGood = () => {
+    incrementGood = (event) => {
         this.setState((prevState) => {
+            
             return {
                 good: prevState.good + 1,
+               
             }
         })
+        
     }
     incrementNeutral = () => {
         this.setState((prevState) => {
@@ -47,22 +52,26 @@ class Statistics extends React.Component {
 
         return <div>
             <h3 className={css.title}>{this.props.naes}</h3>
-            {/* <FeedbackOptions options={} onLeaveFeedback={}></FeedbackOptions> */}
+            {/* <FeedbackOptions incrementBad1 = {this.incrementBad} 
+           incrementGood1={this.incrementGood}
+           incrementNeutral1={this.incrementNeutral}
+
+           /> */}
             <div className={css.btn__list}>
                 <button className={css.btn} onClick={this.incrementGood}>Good</button>
-                <button className={css.btn} onClick={this.incrementNeutral}>Bad</button>
-                <button className={css.btn} onClick={this.incrementBad}>Neutral</button>
+                <button className={css.btn} onClick={this.incrementBad}>Bad</button>
+                <button className={css.btn} onClick={this.incrementNeutral}>Neutral</button>
                 <button onClick={this.toggleMenu}>togle</button>
             </div>
             <h3 className={css.title}>Statistics</h3>
             <ul className={css.list}>
                 <li className={css.item}>Good:  {this.state.good}</li>
-                <li className={css.item}>Bad:  {this.state.neutral}</li>
-                <li className={css.item}>Neutral:  {this.state.bad}</li>
+                <li className={css.item}>Bad:  {this.state.bad}</li>
+                <li className={css.item}>Neutral:  {this.state.neutral}</li>
                 <li className={css.item}>Total: {this.state.good + this.state.neutral + this.state.bad}</li>
                 {/* {this.state.open && */}
-                    <li className={css.item}>Positive feedback: {(this.state.good / (this.state.good + this.state.neutral + this.state.bad) * 100).toFixed()}%</li>
-                {/* } */}
+                    <li className={css.item}>Positive feedback: {this.feedback} {(this.state.good / (this.state.good + this.state.neutral + this.state.bad) * 100).toFixed()}%</li>
+              {/* } */}
             </ul>
 
         </div>
